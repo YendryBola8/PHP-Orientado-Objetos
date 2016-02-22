5317dd267b795bdfb0b98276713ce14af3085474
Yendry <jendry0017@gmail.com>
2016-02-22 15:25:51 -0430

    Clase nº 3 de codigo facilito

--- index.php
+++ index.php
@@ -6,17 +6,38 @@
 	class Persona
 	{
 		// Atributos
-		public $nombre = "Yendry";
+		public $nombre = array();//"Yendry";
+		public $apellido = array();//"Yendry";
 
 		// _Metodos
-		public function hablar($mensaje)
+		public function guardar($nombre, $apellido)
 		{
-			echo $mensaje;
+			$this->nombre[] = $nombre;
+			$this->apellido[] = $apellido;
+		}
+		public function mostrar()
+		{
+			for ($i=0; $i < count($this->nombre); $i++) { 
+				# code...
+				self::formato($this->nombre[$i], $this->apellido[$i]);
+				Persona::formato($this->nombre[$i], $this->apellido[$i]);
+				$this->formato($this->nombre[$i], $this->apellido[$i]);
+			}
+		}
+
+		public function formato($nombre, $apellido)
+		{
+			echo "Nombre: ".$nombre." Apellido: ".$apellido;
 		}
 
 	}
 
 	$persona = new Persona();
-	//echo $persona->nombre;
-	$persona->hablar("Hola que tal");
+	$persona->guardar("1234 sdfgsdg","dgseyrfgwerg 564684");
+	$persona->guardar("1234 sdfgsdg","dgseyrfgwerg 564684");
+	$persona->guardar("1234 sdfgsdg","dgseyrfgwerg 564684");
+	$persona->guardar("1234 sdfgsdg","dgseyrfgwerg 564684");
+	$persona->guardar("1234 sdfgsdg","dgseyrfgwerg 564684");
+	$persona->guardar("1234 sdfgsdg","dgseyrfgwerg 564684");
+	$persona->mostrar();
 ?>
